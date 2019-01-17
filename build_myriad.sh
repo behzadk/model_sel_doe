a7358dbc5518f1de4c8c1910f228e126fd3bd4d6
Behzad Karkaria <ucbtbdk@login12.myriad.ucl.ac.uk>
2019-01-17 10:39:33 +0000

    updated build_myriad

--- build_myriad.sh
+++ build_myriad.sh
@@ -1,45 +1,40 @@
-#!/bin/bash
+#!/bin/bash -l
+#!/usr/bin/env python
 
-# export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/usr/include/python3.6m/"
-# export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/home/behzad/Documents/barnes_lab/cplusplus_software/boost_1_61_0/boost/"
-export PGI=/opt/pgi;
-export PATH=/opt/pgi/linux86-64/18.4/bin:$PATH;
-export MANPATH=$MANPATH:/opt/pgi/linux86-64/18.4/man;
-export LM_LICENSE_FILE=$LM_LICENSE_FILE:/opt/pgi/license.dat;
+cpp_dir=$HOME'/software/cpp_consortium_sim/model_sel_doe/'
+venv=$cpp_dir'venv/cpp_py/bin/activate'
 
-boost_path="/home/behzad/Documents/barnes_lab/cplusplus_software/boost_1_61_0/"
-pyconfig_path="/usr/include/python3.6m/"
+module unload compilers
+module load compilers/gnu/4.9.2
+module load python3/recommended
+module load boost/1_63_0/gnu-4.9.2
 
-py3lib="/usr/lib/x86_64-linux-gnu/libboost_python-py36.so"
-py3libboost_system="/usr/lib/x86_64-linux-gnu/libboost_system.so.1.65.1"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ucbtbdk/Scratch/cpp_consortium_sim/model_sel_doe
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ucbtbdk/software/boost_1_63_0/libs
 
-out_file_name="test_out"
+echo $LD_LIBRARY_PATH
 
-# Build including folder
-# g++ -std=c++11 lv_boost.cpp -I ${boost_path} -I ${pyconfig_path} -L ${py3lib}  -L ${py3libboost_system}  -o lv_exe
-# g++ -std=c++11 test.cpp -I ${boost_path} -I ${pyconfig_path} -L ${py3lib}  -L ${py3libboost_system} -o ${out_file_name} -c
-# g++ -std=c++11 test_boost.cpp -I ${boost_path} -I ${pyconfig_path} -L ${py3lib}  -L ${py3libboost_system} -o ${out_file_name}.so
+# echo $venv
+# source $venv
+python3 -V
+
+# 1. Force bash as the executing shell.
+#$ -S /bin/bash
+
+# 2. Request ten minutes of wallclock time (format hours:minutes:seconds).
+#$ -l h_rt=0:10:0
+
+# 3. Request 1 gigabyte of RAM (must be an integer)
+#$ -l mem=1G
+
+# 5. Set the name of the job.
+#$ -N test_cpp
+
+g++ -std=c++11 -g -shared -o population_modules.so -Wall -fPIC -fopenmp  particle_sim_opemp.cpp model.cpp distances.cpp population.cpp -lboost_system -lboost_python -lpython3.6m -I/shared/ucl/apps/python/bundles/python3-3.0.0/venv/include/python3.6m/
+
+# ./build.sh
+# python3 run_boost_rpr.py
 
 
-
-# g++ -shared -o lv_c_mod.so -fPIC lv_boost.cpp -lboost_python-py36 -lpython3.6m -I/usr/include/python3.6m/
-# g++ -shared -o lv_openmp_mod.so -Wall -fPIC -fopenmp lv_boost_openmp.cpp -lboost_system -lboost_python-py36 -lpython3.6m -I/usr/include/python3.6m/
-# g++  -o particle -Wall -fPIC -fopenmp particle_sim_opemp.cpp -lboost_system -lboost_python-py36 -lpython3.6m -I/usr/include/python3.6m/
-# g++  -std=c++11 -o particle -Wall -fPIC -fopenmp main.cpp model.cpp -lboost_system -lboost_python-py36 -lpython3.6m -I/usr/include/python3.6m/ 
-
-
-# g++  -std=c++1z -shared -o particle_sim.so -Wall -fPIC -fopenmp particle_sim_opemp.cpp model.cpp -lboost_system -lboost_python-py36 -lpython3.6m -I/usr/include/python3.6m/ 
-# g++  -std=c++11 -shared -o particle_sim.so -Wall -fPIC  -fopenmp particle_sim_opemp.cpp model.cpp  -lboost_system -lboost_python-py36 -lpython3.6m -I/usr/include/python3.6m/ 
- 
-g++ -std=c++11 -g -shared -o population_modules.so -Wall -fPIC -fopenmp -fopt-info-note-omp particle_sim_opemp.cpp model.cpp distances.cpp population.cpp -lboost_system -lboost_python3 -lpython3.6m -I/usr/include/python3.6m/ 
-
-
-
-
-# pgc++ -shared -o particle_sim.so particle_sim_opemp.cpp model.cpp -acc -Minfo=accel -lboost_system -lboost_python-py36 -lpython3.6m -I/usr/include/python3.6m/ 
-
-# sudo /opt/pgi/linux86-64/2018/bin/makelocalrc -o /opt/pgi/linux86-64/18.4
-# sudo /opt/pgi/linux86-64/2018/bin/makelocalrc -o /opt/pgi/linux86-64/18.4/
-
-# Run test app
-# ./${out_file_name}
+echo "finished"
